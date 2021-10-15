22ed41256df98838bd210a3a30ec8269a5b0c35e
Perry Raskin <perryraskin@gmail.com>
2021-10-14 23:25:37 -0400

    Homepage update to submit tracking

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -11,17 +11,71 @@ interface Props {}
 const Home: NextPage<Props> = ({}) => {
   return (
     <div className="text-center">
-      <h1>Next.js TailwindCSS Starter</h1>
-      <p>A super simple boilerplate for your Next.js web app</p>
-      <a
-        href="https://github.com/perryraskin/nextjs-tailwindcss-starter"
-        target="_blank"
-      >
-        <Button
-          text="Get Source Code"
-          extend="bg-blue-600 hover:bg-blue-500 text-white"
-        />
-      </a>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Full Stack Track
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Start by entering a tracking number.
+            </p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Carrier
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Tracking #
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    htmlFor="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Notes
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
